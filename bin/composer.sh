13c41b4d251abe5f2eee1a836d427cd1d7d98997
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2016-05-06 00:28:38 +0200

    Allow any command for composer.sh

--- bin/composer.sh
+++ bin/composer.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "--install" ]
-then
-  sh ./bin/docker.sh --build
-  docker-compose up -d application
-  docker run --volumes-from $(docker-compose ps -q application | head -1) mostad/composer install
-  sh ./bin/docker.sh --start
-elif [ "$1" = "--update" ]
-then
-  sh ./bin/docker.sh --build
-  docker-compose up -d application
-  docker run --volumes-from $(docker-compose ps -q application | head -1) mostad/composer update
-  sh ./bin/docker.sh --start
-fi
+docker-compose up -d application
+docker run --volumes-from $(docker-compose ps -q application | head -1) mostad/composer $@
+sh ./bin/docker.sh --start
