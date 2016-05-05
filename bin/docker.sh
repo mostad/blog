6fb2560a4eb9fb9c4fef3fcbf66485f0c1ee9877
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2016-05-05 14:20:06 +0200

    Basic shell scripts

---
+++ bin/docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "--build" ]
+then
+  docker-compose rm --all --force application
+  docker-compose build application
+elif [ "$1" = "--start" ]
+then
+  docker-compose up -d
+fi
