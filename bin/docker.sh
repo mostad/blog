e4b9c4f60ae5e3b5e5ca18c00769ff86773df9f6
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2016-05-14 12:28:27 +0200

    Renamed commands

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "--build" ]
+if [ "$1" = "build" ]
 then
   docker-compose rm --all --force application
   docker-compose build application
-elif [ "$1" = "--start" ]
+elif [ "$1" = "start" ]
 then
   docker-compose up -d
 fi
