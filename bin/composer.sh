2b5d9b00b88d4948edf826cb81b7d2cab94fd98e
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2016-05-14 12:28:45 +0200

    Use composer from application container

--- bin/composer.sh
+++ bin/composer.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose up -d application
-docker run --volumes-from $(docker-compose ps -q application | head -1) mostad/composer $@
-sh ./bin/docker.sh --start
+docker exec $(docker-compose ps -q application | head -1) composer $@
